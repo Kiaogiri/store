ef99b4c74ef748334dd482d94628a95d411e6af8
Kiaogiri <kiaogiri123@gmail.com>
2025-03-09 16:37:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website is ready!");
+
+    function goToBeranda() {
+        window.location.href = "beranda.html"; 
+    }
+
+    let batalButton = document.querySelector(".cancel-button");
+    if (batalButton) {
+        batalButton.addEventListener("click", goToBeranda);
+    }
 });
